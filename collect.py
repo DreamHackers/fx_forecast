0839d2ae7f050520cb9c42fcc05b95e7a26c14cf
kouares <ares.fanovaru@gmail.com>
2016-03-01 01:53:14 +0900

    get forecast table

--- collect.py
+++ collect.py
@@ -18,9 +18,19 @@ class Collector:
     async def scraping(self, url):
         requst = urllib.request.Request(url)
         html = urllib.request.urlopen(requst).read()
-        print("{0} download finish".format(url))
+        print('{0} download finish'.format(url))
         bs = BeautifulSoup(html, "lxml")
-        print(bs.title.string)
+
+        days_forecast_table = bs.find('strong', string='各社別本日のドル円予想レンジ').parent.parent.parent.parent.parent
+        table_file = open('./table_row.txt', 'w')
+
+        table_body = days_forecast_table.find('tbody')
+        table_rows = table_body.find_all('tr')
+        for row in table_rows:
+            table_file.write(row.prettify())
+            table_file.write('\nROW------------------------------------------------------------------------------------------------------------------------------------------------------\n')
+
+        print('scraping finish')
         return html
 
 if __name__ == "__main__":
